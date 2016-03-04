8f9386b545efa43d40f7f02e38d5a5ad0307b672
Igor Lukanin <mail@igor.lukanin.name>
2016-03-04 05:37:03 +0500

    Periodic updates

--- src/app.js
+++ src/app.js
@@ -13,46 +13,52 @@ var config = require('config'),
     url = config.get('repo.url'),
     remote = config.get('repo.remote'),
     message = config.get('repo.message'),
-    privateKey = config.get('repo.privateKeyFile');
+    privateKey = config.get('repo.privateKeyFile'),
+
+    updateInterval = config.get('update_interval_minutes') * 60 * 1000 /* milliseconds */;
 
 
-var distance = strava
-    .loadAthleteInfo()
-    .then(function(info) {
-        return strava.loadAthleteStats(info);
-    })
-    .then(function(stats) {
-        return Math.round(stats.ytd_run_totals.distance / 1000);
-    });
+var update = function() {
+    var distance = strava
+        .loadAthleteInfo()
+        .then(function(info) {
+            return strava.loadAthleteStats(info);
+        })
+        .then(function(stats) {
+            return Math.round(stats.ytd_run_totals.distance / 1000);
+        });
 
-var repo = rmrf(directory)
-    .then(function() {
-        return git.clone(url, directory);
-    });
+    var repo = rmrf(directory)
+        .then(function() {
+            return git.clone(url, directory);
+        });
 
-Promise.all([ distance, repo ])
-    .then(function(results) {
-        var distance = results[0];
+    Promise.all([ distance, repo ])
+        .then(function(results) {
+            var distance = results[0];
 
-        if (distance == yaml.readKey(path, entry)) {
-            return Promise.reject(distance);
-        }
-        else {
-            yaml.updateKey(path, entry, distance);
-            return Promise.resolve(distance);
-        }
-    })
-    .then(function() {
-        return git.add(directory, file);
-    })
-    .then(function() {
-        return git.commit(directory, message);
-    })
-    .then(function() {
-        return git.push(directory, remote, privateKey);
-    })
-    .then(function() {
-        console.log('Distance updated.');
-    }, function() {
-        console.log('Distance not updated.');
-    });
+            if (distance == yaml.readKey(path, entry)) {
+                return Promise.reject(distance);
+            }
+            else {
+                yaml.updateKey(path, entry, distance);
+                return Promise.resolve(distance);
+            }
+        })
+        .then(function() {
+            return git.add(directory, file);
+        })
+        .then(function() {
+            return git.commit(directory, message);
+        })
+        .then(function() {
+            return git.push(directory, remote, privateKey);
+        })
+        .then(function() {
+            console.log('Distance updated.');
+        }, function() {
+            console.log('Distance not updated.');
+        });
+};
+
+setInterval(update, updateInterval);
